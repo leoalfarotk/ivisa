183afd90fbe6e3910572abc46d0ac1c40a6b295c
Leo Alfaro <leonardo.alfaro@tektonlabs.com>
2019-12-12 09:59:21 -0500

    Prod config

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,7 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/.well-known/acme-challenge/{id}', function () {
-    return 'L_Dpj4hF3U8JHZG5x6024Y2BxXwXboSHiMgywkuCoi8.G6G9g1KM-6Pj2Y8XMW0OksVuz0bBmj194iu1wbcp2lA';
-});
